cb2295d41c688b33901a60b330b6c4dc82cf1ed2
Dhruv Sikarwar <dhrvskwrr@gmail.com>
2025-08-21 16:05:54 +0530

    pull

--- Calculator.py
+++ Calculator.py
@@ -11,7 +11,7 @@ def extract_numbers_from_prompt(text):
 def add(*args):return sum(args)
 def subtract(*args):
     result=args[0]
-    for i in args:
+    for i in args[1:]:
         result-=i
     return result
 def multiply(*args):
@@ -25,7 +25,7 @@ def division(*args):
     if not args:
         return "No numbers are given"
     result=args[0]
-    for i in args:
+    for i in args[1:]:
         result/=i
     return result
 def exponentiation(a,b):return a**b
@@ -61,13 +61,14 @@ def hcf(*args):
         result=hcfa(result,i)
     return result
 def factorial(a):
-    if a==0 and a==1:
+    if a==0 or a==1:
         return 1
     n=1
     while a!=1:
         n=n*a
         a-=1
     return n
+
 def sorry():
     print("Sorry I couldn't understand what you wrote please check your prompt and try again.")
 def end():
@@ -77,37 +78,44 @@ def end():
 
 operations1={'END':end,'TERMINATE':end,'EXIT':end}
 operations2={'FACTORS':factorial,'FACTORIAL':factorial,'SQUAREROOT':squareroot}
-
+operations0={'+':add,'-':subtract,'*':multiply,'/':division}
 operations3={'ADD':add,'PLUS':add,'SUM':add,'SUBTRACT':subtract,'MINUS':subtract,'MULTIPLY':multiply,'INTO':multiply,'DIVIDE':division,'POWER':exponentiation,'EXPONENTIAL':exponentiation,'LCM':lcm,'HCF':hcf}
 
 def main():
     print("WelCome to the Smart-Calculator")
     while True:
         text=input("Enter your problem:\n")
-        for word in text.split(" "):
-            if word.upper() in operations3.keys():
-                try:
-                    l=extract_numbers_from_prompt(text)
-                    r=operations3[word.upper()](*l)
-                    print(r)
-                except:
-                    print("Something is Wrong")
-                finally:
-                    break
-            elif word.upper() in operations2.keys():
-                try:
-                    l=extract_numbers_from_prompt(text)
-                    r=operations2[word.upper()](l[0])
-                    print(r)
-                except:
-                    print("Sorry you cannot operate this operation for different operations at same time.")
-                finally:
-                    break
-            elif word.upper() in operations1.keys():
-                    l=extract_numbers_from_prompt(text)
-                    r=operations1[word.upper()]()
-        else:
-            sorry()
-            break
+        if True:
+            for word in text.split(" "):
+                if word.upper() in operations3.keys():
+                    try:
+                        l=extract_numbers_from_prompt(text)
+                        r=operations3[word.upper()](*l)
+                        print(r)
+                    except:
+                        print("Something is Wrong")
+                    finally:
+                        break
+                elif word.upper() in operations2.keys():
+                    try:
+                        l=extract_numbers_from_prompt(text)
+                        r=operations2[word.upper()](l[0])
+                        print(r)
+                    except:
+                        print("Sorry you cannot operate this operation for different operations at same time.")
+                    finally:
+                        break
+                elif word.upper() in operations1.keys():
+                        l=extract_numbers_from_prompt(text)
+                        r=operations1[word.upper()]()
+                else:
+                    operators(text)
+            else:
+                sorry()
+                           
+                
+                
+    
+            
 if __name__=="__main__":
     main()
